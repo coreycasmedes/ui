778d63a51a442af2152592e36b4406e4beed030a
Corey Casmedes <ccasmedes3@gmail.com>
2025-04-07 01:17:49 -0500

    add icons to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,44 +1,23 @@
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+
+const icons = [
+  { href: "https://www.instagram.com/coreycasmedes/", icon: FaInstagram },
+  { href: "https://www.instagram.com/coreycasmedes/", icon: FaGithub },
+  { href: "https://www.instagram.com/coreycasmedes/", icon: FaLinkedin },
+];
 
 export const Footer = () => {
   return (
-    <div className="w-full  flex justify-center flex-shrink-0 ">
-      <div className="flex items-center pl-0 lg:pl-8">
-        <a href="https://www.instagram.com/coreycasmedes/" className="px-2">
-          <FaInstagram />
-          {/* <img
-            src={FaInstagram}
-            className="rounded-full w-8 lg:w-9 border-white border bg-white"
-          ></img> */}
-        </a>
-      </div>
-
-      {/* <div className="flex items-center pl-0">
-        <a href="https://www.instagram.com/coreycasmedes/" className="px-2">
-          <img
-            src={GithubIcon}
-            className="rounded-full w-8 lg:w-9 border-white border bg-white"
-          ></img>
-        </a>
-      </div>
-
-      <div className="flex items-center pl-0 fill-white">
-        <a href="https://www.instagram.com/coreycasmedes/" className="px-2">
-          <img
-            src={LinkedInIcon}
-            className="rounded-full w-8 lg:w-9 border-white border bg-white"
-          ></img>
-        </a>
-      </div>
-
-      <div className="flex items-center pl-0 ">
-        <a href="https://www.instagram.com/coreycasmedes/" className="px-2">
-          <img
-            src={EmailIcon}
-            className="rounded-full w-8 lg:w-9 border-white border"
-          ></img>
-        </a>
-      </div> */}
+    <div className="w-full flex justify-center flex-shrink-0 p-2">
+      {icons.map(({ href, icon: Icon }) => (
+        <div className="flex items-center">
+          <a href={href} className="px-2">
+            <div className="rounded-full p-1  border-white border">
+              <Icon className="h-6 w-6 text-white" />
+            </div>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
