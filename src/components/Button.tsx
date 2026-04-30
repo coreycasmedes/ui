5283c8e13b50a0784aad85660cebe9b031bae24d
Corey Casmedes <ccasmedes3@gmail.com>
2026-04-29 22:38:49 -0500

    chore: remove unused components

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-export const Button = () => {
-  return (
-    <button className="px-6 py-2 mt-12 bg-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
-      Figma
-    </button>
-  );
-};
